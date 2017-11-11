bf9671986dd9a12e9779dd1c1b8b1aa537139c7d
Rasmus Järvelä <jarvela.rasmus@gmail.com>
2017-11-11 19:49:48 +0100

    Add dwarf control interface

---
+++ Assets/Scripts/IDwarfControl.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDwarfControl {
+	void Jump ();
+	void Move (float horizontal);
+	void Attack ();
+}
