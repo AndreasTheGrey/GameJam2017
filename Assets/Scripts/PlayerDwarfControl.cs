9e3217c60d5f1b28ffadf9cf83cd2485d51bd83e
Rasmus Järvelä <jarvela.rasmus@gmail.com>
2017-11-11 21:21:55 +0100

    Add player dwarf control script

---
+++ Assets/Scripts/PlayerDwarfControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDwarfControl : MonoBehaviour {
+
+	public IDwarfControl dwarfControl;
+	public int playerNum = 1;
+
+	void Start () {
+	}
+
+	void Update () {
+		if (Input.GetButtonDown(appendPlayerSuffix("Jump")))
+        {
+			dwarfControl.Jump ();
+		}
+		if (Input.GetButtonDown(appendPlayerSuffix("Attack")))
+		{
+			dwarfControl.Attack ();
+		}
+	}
+
+	void FixedUpdate () {
+		dwarfControl.Move (Input.GetAxis(appendPlayerSuffix("Horizontal")), Input.GetAxis(appendPlayerSuffix("Vertical")));
+	}
+
+	private string appendPlayerSuffix(string name) {
+		return name + "_P" + playerNum;
+	}
+}
