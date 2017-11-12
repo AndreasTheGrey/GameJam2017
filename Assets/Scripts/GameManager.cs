d741f3acd860a754668fdff18125f84277d5e70a
Rasmus Järvelä <jarvela.rasmus@gmail.com>
2017-11-12 11:05:20 +0100

    Work on menu and map

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    //private FightManager fightManager;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);    
+
+        DontDestroyOnLoad(gameObject);
+
+        //fightManager = GetComponent<FightManager>();
+
+        InitGame();
+    }
+
+    void InitGame() {
+        SceneManager.LoadScene("Menu", LoadSceneMode.Additive);
+    }
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
