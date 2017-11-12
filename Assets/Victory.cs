a34efdb803e57dc13eb3fe945ba7984a9bf87315
Rasmus Järvelä <jarvela.rasmus@gmail.com>
2017-11-12 16:05:33 +0100

    Add more stuff

---
+++ Assets/Victory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Victory : MonoBehaviour {
+
+    public Sprite[] sprites;
+
+    void Start () {
+        if (GameManager.Instance.winner != null) {
+            Debug.Log (GameManager.Instance.winner);
+            if (GameManager.Instance.winner == "Player 1") {
+                GetComponent<Image> ().overrideSprite = sprites [0];
+            } else if (GameManager.Instance.winner  == "Player 2") {
+                GetComponent<Image> ().overrideSprite = sprites [1];
+            } else if (GameManager.Instance.winner  == "Player 3") {
+                GetComponent<Image> ().overrideSprite = sprites [2];
+            } else if (GameManager.Instance.winner  == "Player 4") {
+                GetComponent<Image> ().overrideSprite = sprites [3];
+            }
+        }
+	}
+	
+	void Update () {
+	}
+}
